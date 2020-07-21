eaa8772fd9dc4bcbf906cc2aaa24c27c562c5542
riyak1998@gmail.com <aishanya11@gmail.com>
2020-07-22 01:14:46 +0530

    Adding chatsToJson

---
+++ Website-Code/chatsToJson/txt-json.py
@@ -0,0 +1,10 @@
+from whatsapptojson import whatsapptojson
+
+txt_file = "./chats.txt" 
+output_json = "./chats.json"
+whatsapptojson.format_file(source = txt_file, destination = output_json, device = 'android')
+from whatsapptojson import whatsapptojson
+
+txt_file = "/chats1.txt" 
+output_json = "/chats.json"
+whatsapptojson.format_file(source = txt_file, destination = output_json, device = 'android')
